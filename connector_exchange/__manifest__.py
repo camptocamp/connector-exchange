f0702f66580188e4a61d1330e9dc147e50af0389
Damien Crier <damdam-s@users.noreply.github.com>
2019-04-16 15:15:12 +0200

    change version because of the library change

--- connector_exchange/__manifest__.py
+++ connector_exchange/__manifest__.py
@@ -6,7 +6,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {'name': 'Connector Exchange',
- 'version': '10.0.1.0.0',
+ 'version': '10.0.2.0.0',
  'author': "Camptocamp, Nicolas Clavier, Odoo Community Association (OCA)",
  'website': "http://www.camptocamp.com",
  'summary': "Microsoft Exchange Connector",
